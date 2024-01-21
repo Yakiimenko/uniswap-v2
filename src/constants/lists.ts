90dea7b293f60bbc6eed8622c6d4bbd997a32a4b
marinseremet12 <iuliiagotess@gmail.com>
2024-01-21 22:58:23 +0300

    initial commit

---
+++ src/constants/lists.ts
@@ -0,0 +1,12 @@
+// the Uniswap Default token list lives here
+export const DEFAULT_TOKEN_LIST_URL = 'tokens.uniswap.eth'
+
+export const DEFAULT_LIST_OF_LISTS: string[] = [
+  'https://app.tryroll.com/tokens.json',
+  'https://raw.githubusercontent.com/compound-finance/token-list/master/compound.tokenlist.json',
+  'https://defiprime.com/defiprime.tokenlist.json',
+  'https://umaproject.org/uma.tokenlist.json'
+]
+
+
+
